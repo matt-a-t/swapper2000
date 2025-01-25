b5c0d5b9b09d060f025d9120b58e96dba247cb68
matt <matt.armstro@gmail.com>
2025-01-25 10:39:09 -0500

    landing page progress

--- app.vue
+++ app.vue
@@ -1,6 +1,16 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="container">
+    <NuxtPage />
   </div>
 </template>
+
+<script>
+import "~/assets/baseline.css";
+</script>
+
+<style>
+.container {
+  background: linear-gradient(to right, #0fe5e5 0%, #00ff00 100%);
+  min-height: 100vh;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <Header>SWAPPER 2000</Header>
+    <img src="/garf-logo.png" alt="Garfield Swapper 2000" />
+    <Button>ENTER</Button>
+  </main>
+</template>
+
+<style>
+main {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+}
+
+main img {
+  width: 50%;
+  /* max-width: 400px; */
+}
+</style>
