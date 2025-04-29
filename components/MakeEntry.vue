602006eebe7fc7a56fe9eb1618994f0afe25a99c
matt <matt.armstro@gmail.com>
2025-04-28 22:45:40 -0400

    change comment input from text field to textarea for improved user experience

--- components/MakeEntry.vue
+++ components/MakeEntry.vue
@@ -48,7 +48,7 @@ async function handleSubmit(event) {
     <input id="link" name="link" type="text" />
 
     <label for="comment">Comment</label>
-    <input id="comment" name="comment" type="textarea" />
+    <textarea id="comment" name="comment" type="text" />
 
     <input type="submit" value="SWAP!" />
   </form>
